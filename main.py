0497c402719ee8149c69f0080f99ecd55c3831cc
Celshade <ggcelshade@gmail.com>
2026-01-29 18:53:32 -0500

    Breakout Player into its own module; update TODO

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import random
 
 import pygame
 
-from config import *
+from config import *  # TODO refine
+from player import Player
 
 
 # =================== #
@@ -44,28 +45,6 @@ def main() -> None:
 # ============================================================================
 # GAME OBJECTS
 # ============================================================================
-class Player:
-    def __init__(self):
-        self.x = PLAYER_START_X
-        self.y = PLAYER_START_Y
-        self.vel = 0
-        self.size = PLAYER_SIZE
-
-    def update(self):
-        self.vel += GRAVITY
-        self.y += self.vel
-
-    def keypress(self):
-        self.vel = KEY_POWER
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, YELLOW, (self.x, self.y, self.size, self.size))
-        pygame.draw.rect(
-            screen, BLACK, (self.x, self.y, self.size, self.size), 2
-        )
-
-    def is_dead(self):
-        return self.y + self.size >= GROUND_Y or self.y < 0
 
 class Pipe:
     def __init__(self, x):
---
+++ player.py
@@ -0,0 +1,27 @@
+import pygame
+
+from config import *
+
+
+class Player:
+    def __init__(self):
+        self.x = PLAYER_START_X
+        self.y = PLAYER_START_Y
+        self.vel = 0
+        self.size = PLAYER_SIZE
+
+    def update(self):
+        self.vel += GRAVITY
+        self.y += self.vel
+
+    def keypress(self):
+        self.vel = KEY_POWER
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, YELLOW, (self.x, self.y, self.size, self.size))
+        pygame.draw.rect(
+            screen, BLACK, (self.x, self.y, self.size, self.size), 2
+        )
+
+    def is_dead(self):
+        return self.y + self.size >= GROUND_Y or self.y < 0
