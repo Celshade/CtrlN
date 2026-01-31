c143b99dc0218a399c61272413d1f7f3bc2e3e41
Celshade <ggcelshade@gmail.com>
2026-01-31 11:31:55 -0500

    Implement actual game assets

--- objects.py
+++ objects.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 import pygame
 
@@ -17,20 +18,27 @@ class Pipe:
         self.gap_start = random.randint(min_gap, max_gap)
         self.gap_end = self.gap_start + PIPE_GAP
         self.scored = False
+        
+        # Load pipe sprite
+        asset_path = os.path.join(os.path.dirname(__file__), 'assets', 'pipe.png')
+        self.pipe_image = pygame.image.load(asset_path)
+        # Scale the pipe image to fit the PIPE_WIDTH
+        self.pipe_image = pygame.transform.scale(
+            self.pipe_image, (int(PIPE_WIDTH), int(WINDOW_HEIGHT))
+        )
 
     def update(self):
         self.x += PIPE_SPEED
 
     def draw(self, screen):
-        # Top pipe
-        pygame.draw.rect(screen, GREEN, (self.x, 0, PIPE_WIDTH, self.gap_start))
-        pygame.draw.rect(screen, BLACK,
-                         (self.x, 0, PIPE_WIDTH, self.gap_start), 2)
-        # Bottom pipe
-        pygame.draw.rect(screen, GREEN,
-            (self.x, self.gap_end, PIPE_WIDTH, GROUND_Y - self.gap_end))
-        pygame.draw.rect(screen, BLACK,
-            (self.x, self.gap_end, PIPE_WIDTH, GROUND_Y - self.gap_end), 2)
+        # Top pipe - scale to gap_start height
+        top_pipe = pygame.transform.scale(self.pipe_image, (int(PIPE_WIDTH), int(self.gap_start)))
+        screen.blit(top_pipe, (self.x, 0))
+        
+        # Bottom pipe - scale to the height needed
+        bottom_height = GROUND_Y - self.gap_end
+        bottom_pipe = pygame.transform.scale(self.pipe_image, (int(PIPE_WIDTH), int(bottom_height)))
+        screen.blit(bottom_pipe, (self.x, self.gap_end))
 
     def is_off_screen(self):
         return self.x < -PIPE_WIDTH
