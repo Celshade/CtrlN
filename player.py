1b401c3c2da999d8226459c23eb623eb2a4fcc23
Celshade <ggcelshade@gmail.com>
2026-02-01 06:11:51 -0500

    Add annotations to Player() and adjust var names

--- player.py
+++ player.py
@@ -7,27 +7,27 @@ from config import *
 # ### Player Class ### #
 # ==================== #
 class Player:
-    def __init__(self):
-        self.x = PLAYER_START_X
-        self.y = PLAYER_START_Y
+    def __init__(self) -> None:
+        self.x_pos = PLAYER_START_X
+        self.y_pos = PLAYER_START_Y
         self.vel = 0
         self.size = PLAYER_SIZE
 
         # Load player sprite
         self.image = pygame.image.load("assets/player.png")
         self.image = pygame.transform.scale(self.image, (self.size, self.size))
-        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        self.rect = self.image.get_rect(topleft=(self.x_pos, self.y_pos))
 
-    def update(self):
+    def update(self) -> None:
         self.vel += GRAVITY
-        self.y += self.vel
-        self.rect.topleft = (self.x, self.y)
+        self.y_pos += self.vel
+        self.rect.topleft = (self.x_pos, self.y_pos)
 
-    def keypress(self):
+    def keypress(self) -> None:
         self.vel = KEY_POWER
 
-    def draw(self, screen):
+    def draw(self, screen) -> None:
         screen.blit(self.image, self.rect)
 
-    def is_dead(self):
-        return self.y + self.size >= GROUND_Y or self.y < 0
+    def is_dead(self) -> bool:
+        return self.y_pos + self.size >= GROUND_Y or self.y_pos < 0
