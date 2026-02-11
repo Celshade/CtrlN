2423a488492a37751b8d79e18c5fad6e36a98175
Celshade <ggcelshade@gmail.com>
2026-02-11 00:51:39 -0500

    Implement packs of birds (1-3 for now - fairly tight group); 1-2 will clear a shield charge, 3 do not (x-distance vs quantity?)

--- config.py
+++ config.py
@@ -37,6 +37,10 @@ TREE_SIZE = int(PLAYER_SIZE * 1.5)
 TREE_SPAWN_RATE = 60
 TREE_SPACING = int(TREE_SIZE * 0.8)  # Horizontal spacing between trees in a group
 
+# Perched Birds (obstacles on trees)
+PERCHED_BIRD_SIZE = int(PLAYER_SIZE * 1)
+PERCHED_BIRD_SPAWN_CHANCE = 0.4  # 40% chance of a bird on each tree
+
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
