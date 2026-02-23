4564713c9a780315f7add68d29c5dbefd8f7c73b
Celshade <ggcelshade@gmail.com>
2026-02-23 10:11:22 -0500

    Flatten sub-dirs in src that were causing issues; remove unnecessary config files

--- src/clackykey/objects.py
+++ src/clackykey/objects.py
@@ -3,8 +3,8 @@ import random
 import pygame
 from PIL import Image
 
-from ..core.player import Player
-from ..config import (
+from clackykey.player import Player
+from clackykey.config import (
     GROUND_Y, ORB_SIZE, OBJECT_SPEED, ORB_MIN_MARGIN, ORB_MAX_MARGIN, ORANGE,
     TREE_SIZE, OBJECT_SPEED, PERCHED_BIRD_SIZE
 )
--- src/clackykey/systems/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Game systems (counter, objects, etc)."""
-
-from .counter import Counter
-from .objects import Orb, Tree, BirdPerched
-
-__all__ = ["Counter", "Orb", "Tree", "BirdPerched"]
